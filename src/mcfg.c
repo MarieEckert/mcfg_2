c961738ac0a9c7112fa1544e83dc19e23934c4ed
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-12-25 23:02:32 +0100

    read input file and add parse_ctxt structure

--- src/mcfg.c
+++ src/mcfg.c
@@ -1,17 +1,69 @@
+// mcfg.c ; marie config format parser implementation
+// implementation for mcfg version 2
+//
+// Copyright (c) 2023, Marie Eckert
+// Licensend under the BSD 3-Clause License.
+//------------------------------------------------------------------------------
+
 #include "mcfg.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 char *mcfg_err_string(mcfg_err_t err) {
   return "ficken";
 }
 
-mcfg_err_t mcfg_parse_line(char *line, size_t len, mcfg_file_t *file) {
+mcfg_err_t mcfg_parse_line(char *line, mcfg_parser_ctxt_t *ctxt) {
+  printf("%s\n", line);
   return MCFG_OK;
 }
 
-mcfg_err_t mcfg_parse(char *input, size_t size, mcfg_file_t *file) {
+mcfg_err_t mcfg_parse_file(char *path, mcfg_file_t *file) {
+  FILE *in_file;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  errno = 0;
+  in_file = fopen(path, "r");
+  if (in_file == NULL)
+    return MCFG_OS_ERROR_MASK | errno;
+
+  file->sector_count = 0;
+
+  mcfg_parser_ctxt_t ctxt = {
+    .target_file = file,
+    .target_sector = NULL,
+    .target_section = NULL,
+    .target_field = NULL,
+    .linenum = 0,
+    .file_path = path,
+  };
+  
+  while ((read = getline(&line, &len, in_file)) != -1) {
+    ctxt.linenum++;
+    mcfg_err_t result = mcfg_parse_line(line, &ctxt);
+    if (result != MCFG_OK)
+      return result;
+  }
+
+  fclose(in_file);
+  if (line)
+    free(line);
+
   return MCFG_OK;
 }
 
-mcfg_err_t mcfg_parse_file(char *path, mcfg_file_t *out) {
-  return MCFG_OK;
+void mcfg_free_field(mcfg_field_t *field) {
+}
+
+void mcfg_free_section(mcfg_section_t *section) {
+}
+
+void mcfg_free_sector(mcfg_sector_t *sector) {
+}
+
+void mcfg_free_file(mcfg_file_t *file) {
 }
--- src/mcfg.h
+++ src/mcfg.h
@@ -2,6 +2,7 @@
 #define MCFG_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 typedef enum mcfg_err_t {
   MCFG_OK,
@@ -17,7 +18,7 @@ typedef enum mcfg_field_type_t {
 typedef struct mcfg_field_t {
   char *name;
   mcfg_field_type_t type;
-  char *data;
+  void *data;
   size_t length;
 } mcfg_field_t;
 
@@ -38,12 +39,28 @@ typedef struct mcfg_file_t {
   mcfg_sector_t *sectors;
 } mcfg_file_t;
 
+typedef struct mcfg_parser_ctxt_t {
+  mcfg_file_t *target_file;
+  mcfg_sector_t *target_sector;
+  mcfg_section_t *target_section;
+  mcfg_field_t *target_field;
+
+  uint32_t linenum;
+  char *file_path;
+} mcfg_parser_ctxt_t;
+
 char *mcfg_err_string(mcfg_err_t err);
 
-mcfg_err_t mcfg_parse_line(char *line, size_t len, mcfg_file_t *file);
+mcfg_err_t mcfg_parse_line(char *line, mcfg_parser_ctxt_t *ctxt);
 
-mcfg_err_t mcfg_parse(char *input, size_t size, mcfg_file_t *file);
+mcfg_err_t mcfg_parse_file(char *path, mcfg_file_t *file);
 
-mcfg_err_t mcfg_parse_file(char *path, mcfg_file_t *out);
+void mcfg_free_field(mcfg_field_t *field);
+
+void mcfg_free_section(mcfg_section_t *section);
+
+void mcfg_free_sector(mcfg_sector_t *sector);
+
+void mcfg_free_file(mcfg_file_t *file);
 
 #endif // ifndef MCFG_H
