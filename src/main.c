3346d2e8c967c2390616208ba35bb249e8f35718
FelixEcker <felix0eckert0.online.de@gmail.com>
2024-01-04 21:41:28 +0100

    add file printing function for debugging purposes

--- src/main.c
+++ src/main.c
@@ -3,6 +3,54 @@
 
 #include "mcfg.h"
 
+void print_file(mcfg_file_t *file) {
+  printf("mcfg_file_t {\n");
+  printf("  size_t dynfield_count = %zu\n", file->dynfield_count);
+  printf("  mcfg_field_t *dynfields = [\n");
+  for (size_t i = 0; i < file->dynfield_count; i++) {
+    printf("    %zu = {\n", i);
+    printf("      char *name = %s\n", file->dynfields[i].name);
+    printf("      mcfg_field_type_t type = %d\n", file->dynfields[i].type);
+    printf("      size_t size = %zu\n", file->dynfields[i].size);
+    printf("      void *data = %s\n", "?");
+    printf("    }\n");
+  }
+  printf("  ]\n");
+  printf("  size_t sector_count = %zu\n", file->sector_count);
+  printf("  mcfg_sector_t *sectors = [\n");
+  for (size_t i = 0; i < file->sector_count; i++) {
+    printf("    %zu = {\n", i);
+    printf("      char *name = %s\n", file->sectors[i].name);
+    printf("      size_t section_count = %zu\n",
+           file->sectors[i].section_count);
+    printf("      mcfg_section_t *sections = [\n");
+    for (size_t j = 0; j < file->sectors[i].section_count; j++) {
+      printf("        %zu = {\n", j);
+      printf("          char *name = %s\n", file->sectors[i].sections[j].name);
+      printf("          size_t field_count = %zu\n",
+             file->sectors[i].sections[j].field_count);
+      printf("          mcfg_field_t *fields = [\n");
+      for (size_t k = 0; k < file->sectors[i].sections[j].field_count; k++) {
+        printf("            %zu = {\n", k);
+        printf("              char *name = %s\n",
+               file->sectors[i].sections[j].fields[k].name);
+        printf("              mcfg_field_type_t type = %d\n",
+               file->sectors[i].sections[j].fields[k].type);
+        printf("              size_t size = %zu\n",
+               file->sectors[i].sections[j].fields[k].size);
+        printf("              void *data = %s\n", "?");
+        printf("            }\n");
+      }
+      printf("          ]\n");
+      printf("        }\n");
+    }
+    printf("      ]\n");
+    printf("    }\n");
+  }
+  printf("  ]\n");
+  printf("}\n");
+}
+
 int main(int argc, char **argv) {
   char *filepath = "doc/example.mb";
 
@@ -17,6 +65,7 @@ int main(int argc, char **argv) {
   }
 
   fprintf(stderr, "parsed you a mcfg file!\n");
+  print_file(file);
 cleanup:
   mcfg_free_file(file);
   return 0;
