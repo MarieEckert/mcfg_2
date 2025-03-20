dbf36f30167747a63cf4ae63ca4dcf811402a573
Marie <marie@ha.si>
2025-03-20 22:28:27 +0100

    [tests]: compare original vs new struct in serializer test

--- tests/src/serialize.c
+++ tests/src/serialize.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mcfg.h"
+#include "mcfg_util.h"
 
 #include "testing_shared.c"
 
@@ -48,7 +50,7 @@ char *initial =
 	"  end\n"
 	"end\n";
 
-#define TEST_STEPS 3
+#define TEST_STEPS 4
 
 mcfg_file_t
 test_parse_original()
@@ -94,8 +96,6 @@ test_serialize(mcfg_file_t file)
 		exit(current_step);
 	}
 
-	printf("%s\n", serialized_result.value->data);
-
 	STEP_SUCCESS;
 	return serialized_result.value;
 }
@@ -121,6 +121,144 @@ test_parse_serialized(mcfg_string_t *serialized)
 	return ret.value;
 }
 
+void
+test_compare_structs(mcfg_file_t a, mcfg_file_t b)
+{
+	BEGIN_STEP("comparing original and newly parsed");
+
+	if(a.sector_count != b.sector_count) {
+		STEP_FAIL;
+
+		fprintf(stderr, STEP_LOG_PRIMER "sector count mismatch");
+		exit(current_step);
+	}
+
+	for(size_t ix = 0; ix < a.sector_count; ix++) {
+		mcfg_sector_t a_sector = a.sectors[ix];
+		mcfg_sector_t b_sector = b.sectors[ix];
+
+		if(strcmp(a_sector.name, b_sector.name) != 0) {
+			STEP_FAIL;
+
+			fprintf(stderr, STEP_LOG_PRIMER "sector order / name mismatch!\n");
+			fprintf(stderr, STEP_LOG_PRIMER "\"%s\" vs \"%s\"\n", a_sector.name,
+					b_sector.name);
+			exit(current_step);
+		}
+
+		if(a_sector.section_count != b_sector.section_count) {
+			STEP_FAIL;
+
+			fprintf(stderr,
+					STEP_LOG_PRIMER "section count mismatch in sector \"%s\"",
+					a_sector.name);
+			exit(current_step);
+		}
+
+		for(size_t jx = 0; ix < a_sector.section_count; ix++) {
+			mcfg_section_t a_section = a_sector.sections[jx];
+			mcfg_section_t b_section = b_sector.sections[jx];
+
+			if(strcmp(a_section.name, b_section.name) != 0) {
+				STEP_FAIL;
+
+				fprintf(stderr,
+						STEP_LOG_PRIMER "section order / name mismatch!\n");
+				fprintf(stderr, STEP_LOG_PRIMER "\"%s\" vs \"%s\"\n",
+						a_section.name, b_section.name);
+				exit(current_step);
+			}
+
+			if(a_section.field_count != b_section.field_count) {
+				STEP_FAIL;
+
+				fprintf(stderr,
+						STEP_LOG_PRIMER
+						"field_count count mismatch in sectiob \"%s\"",
+						a_section.name);
+				exit(current_step);
+			}
+
+			for(size_t kx = 0; ix < a_sector.section_count; ix++) {
+				mcfg_field_t a_field = a_section.fields[kx];
+				mcfg_field_t b_field = b_section.fields[kx];
+
+				if(strcmp(a_field.name, b_field.name) != 0) {
+					STEP_FAIL;
+
+					fprintf(stderr,
+							STEP_LOG_PRIMER "field order / name mismatch!\n");
+					fprintf(stderr, STEP_LOG_PRIMER "\"%s\" vs \"%s\"\n",
+							a_field.name, b_field.name);
+					exit(current_step);
+				}
+
+				if(a_field.type != b_field.type) {
+					STEP_FAIL;
+
+					fprintf(stderr,
+							STEP_LOG_PRIMER "field order / type mismatch!");
+					fprintf(stderr, STEP_LOG_PRIMER "%d vs %d\n", a_field.type,
+							b_field.type);
+					exit(current_step);
+				}
+
+				switch(a_field.type) {
+					case TYPE_STRING:
+					case TYPE_LIST:
+						if(strcmp(mcfg_data_as_string(a_field),
+								  mcfg_data_as_string(b_field)) != 0) {
+							STEP_FAIL;
+
+							fprintf(stderr,
+									"field value mismatch for field \"%s\"\n",
+									a_field.name);
+							exit(current_step);
+						}
+						break;
+					case TYPE_BOOL:
+						if(mcfg_data_as_bool(a_field) !=
+						   mcfg_data_as_bool(b_field)) {
+							STEP_FAIL;
+
+							fprintf(stderr,
+									"field value mismatch for field \"%s\"\n",
+									a_field.name);
+							exit(current_step);
+						}
+						break;
+					case TYPE_I8:
+					case TYPE_U8:
+					case TYPE_I16:
+					case TYPE_U16:
+					case TYPE_I32:
+					case TYPE_U32:
+						if(mcfg_data_as_int(a_field) !=
+						   mcfg_data_as_int(b_field)) {
+							STEP_FAIL;
+
+							fprintf(stderr,
+									"field value mismatch for field \"%s\"\n",
+									a_field.name);
+							exit(current_step);
+						}
+						break;
+					case TYPE_INVALID:
+						STEP_FAIL;
+
+						fprintf(stderr,
+								STEP_LOG_PRIMER
+								"invalid type encountered! field \"%s\"\n",
+								a_field.name);
+						exit(current_step);
+				}
+			}
+		}
+	}
+
+	STEP_SUCCESS;
+}
+
 int
 main(void)
 {
@@ -128,7 +266,8 @@ main(void)
 
 	mcfg_file_t parsed = test_parse_original();
 	mcfg_string_t *serialized = test_serialize(parsed);
-	parsed = test_parse_serialized(serialized);
+	mcfg_file_t new_parsed = test_parse_serialized(serialized);
+	test_compare_structs(parsed, new_parsed);
 
 	return 0;
 }
